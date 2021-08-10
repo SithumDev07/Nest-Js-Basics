91fcc3129140964fd93d32fd66dbec4ca661503f
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-10 17:31:42 +0530

    Completed Unit Testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -57,8 +57,59 @@ describe('MoviesService', () => {
         genres: ['test']
       });
 
-      console.log(service.getAll());
+      // const AllMovies = service.getAll();
+      const AllMovies = service.getAll().length;
+      service.deleteOne(1);
+      // const afterDelete = service.getAll();
+      const afterDelete = service.getAll().length;
 
+      // expect(afterDelete.length).toEqual(AllMovies.length - 1);
+      expect(afterDelete).toBeLessThan(AllMovies);
+    })
+
+    it('Should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    })
+
+  })
+
+  describe("Create()", () => {
+    it("Should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        year: 2455,
+        genres: ['test']
+      });
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    })
+  });
+
+  describe("Update()", () => {
+    it("should update a movie", () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2455,
+        genres: ['test']
+      });
+      service.update(1, { title: 'Updated Test' });
+
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    })
+
+    it('Should return a 404', () => {
+      try {
+        service.update(999, { title: 'Updated Test' });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     })
   })
 
