1104c0b37dc4638a5d23f675d459de793cf8a589
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-10 23:21:22 +0530

    Completed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,6 +12,13 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true
+      })
+    )
     await app.init();
   });
 
@@ -19,6 +26,43 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my movie API');
   });
+
+  describe('/movies', () => {
+    // it('GET', () => {
+    //   return request(app.getHttpServer())
+    //     .get('/movies')
+    //     .expect(200)
+    // })
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: "Tenet",
+          year: 2015,
+          genres: ['Thriller, Scientific']
+        })
+        .expect(201);
+    })
+  })
+
+
+  // it('/movies (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/movies')
+  //     .expect(200)
+  //     .expect([]);
+  // })
+
+  describe('/movies/:id', () => {
+    // it.todo('GET')
+    // it.todo('DELETE')
+    // it.todo('PATCH')
+
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    })
+  })
 });
