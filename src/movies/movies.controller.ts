be738bbfef9de07e5e57818fa046b67174d381a1
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-10 12:04:49 +0530

    Basic Requests

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+
+    @Get()
+    getAll() {
+        return "This will retun all movies";
+    }
+
+    @Get('/:id')
+    getOne(@Param("id") id: string) {
+        return `Movie with ${id}`;
+    }
+
+    @Post()
+    create() {
+        return 'THis will create movie';
+    }
+
+    @Delete("/:id")
+    delete(@Param("id") movieId: string) {
+        return `Movie Deleted ${movieId}`;
+    }
+
+    //If we use put it will update the whole movie
+    @Patch('/:id')
+    update(@Param('id') movieId: string) {
+        return `Patched ${movieId}`;
+    }
+}
