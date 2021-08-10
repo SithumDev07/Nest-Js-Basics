c148e0972984588babd797c67a945e479a6de9c3
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-10 17:12:10 +0530

    Unit Testing Upto Get

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,50 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll()", () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2455,
+        genres: ['test']
+      });
+
+      const movie = service.getOne(1);
+
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('Should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie With id:999 not found")
+      }
+    })
+
+  });
+
+  describe('deletOne', () => {
+    it("deletes a movie", () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2455,
+        genres: ['test']
+      });
+
+      console.log(service.getAll());
+
+    })
+  })
+
 });
